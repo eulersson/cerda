18c149600e9e708005794bcc901acc609bb5cc42
Ramon Blanquer <blanquer.ramon@gmail.com>
2016-10-03 20:40:40 +0100

    Updated deploy script

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+git add docs/build/html
+git commit -m "Deploy updated documentation"
+git subtree push --prefix docs/build/html origin gh-pages
+git reset --hard HEAD^
