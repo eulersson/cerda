e80706fce9c8d77e446ef6fd83698cdb97a56981
Ramon Blanquer <blanquer.ramon@gmail.com>
2016-10-03 20:45:27 +0100

    Fix deploy script

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git add docs/build/html
+git add ./docs/build/html
 git commit -m "Deploy updated documentation"
 git subtree push --prefix docs/build/html origin gh-pages
 git reset --hard HEAD^
