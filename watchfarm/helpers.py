3cb99f37edd43725cc624f81a02d7f8be67fc1a3
Ramon Blanquer <blanquer.ramon@gmail.com>
2016-10-04 19:19:43 +0100

    Saving dbox token on user disk

--- watchfarm/helpers.py
+++ watchfarm/helpers.py
@@ -6,23 +6,54 @@ import dropbox
 logger = logging.getLogger("watchfarm")
 
 def dropbox_setup():
-    logger.warn("Might not be fully functioning yet...")
-    app_key = 'gpf11zjrtp5od4x'
-    app_secret = 'cyk4k6zgmraajy7'
+    token_path = os.path.join(os.path.expanduser(os.path.sep.join(['~', '.watchfarm'])), 'dbox_token.txt')
+    logger.debug("Checking if token configuration is saved on disk...")
+    logger.debug("Token location: %s", token_path)
 
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-    authorize_url = flow.start()
+    APP_KEY = 'gpf11zjrtp5od4x'
+    APP_SECRET = 'cyk4k6zgmraajy7'
 
-    logger.info("1. Go to: %s", authorize_url)
-    logger.info("2. Click 'Allow' (you might have to log in first).")
-    logger.info("3. Copy the authorization code.")
+    if os.path.isfile(token_path):
+        logger.debug("IT EXISTS!")
+        logger.debug("Opening token file")
 
-    code = raw_input("Enter the authorization code here: ").strip()
+        token_file = open(token_path)
+        access_token = token_file.read()
+        token_file.close()
 
-    access_token, user_id = flow.finish(code)
+        client = dropbox.client.DropboxClient(access_token)
 
-    client = dropbox.client.DropboxClient(access_token)
-    print 'linked acocunt: ', client.account_info()
+        print "linked account:", client.account_info()
+
+    else:
+        logger.debug("Token file does not exist... Need to create one.")
+
+        flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+        authorize_url = flow.start()
+
+        logger.info("1. Go to: %s", authorize_url)
+        logger.info("2. Click 'Allow' (you might have to log in first).")
+        logger.info("3. Copy the authorization code.")
+
+        code = raw_input("Enter the authorization code here: ").strip()
+
+        access_token, user_id = flow.finish(code)
+
+        logger.debug("Access token is... %s", access_token)
+
+        logger.debug("Writing access token's key and secret to file...")
+
+        token_folder = os.path.dirname(token_path)
+
+        if not os.path.exists(token_folder):
+            os.makedirs(token_folder)        
+
+        token_file = open(token_path, 'w')
+        token_file.write(access_token)
+        token_file.close()
+
+        client = dropbox.client.DropboxClient(access_token)
+        print 'linked acocunt: ', client.account_info()
 
 def get_abs_form_rel(rel_rem_dir, username):
     return os.path.join(
