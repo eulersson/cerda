7a873cd1cd48b6adbcd36b8a1c2b9ca5b6eb0b71
Ramon Blanquer <blanquer.ramon@gmail.com>
2016-10-06 14:29:10 +0100

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ setup(
     version='0.1.0',
     description='An NCCA renderfarm SFTP watcher',
     url='https://github.com/docwhite/cerda',
+    download_url='https://github.com/docwhite/cerda/tarball/0.1.0',
     author='Ramon Blanquer',
     author_email='blanquer.ramon@gmail.com',
     license='MIT',
-    keywords='renderfarm ncca transfer files watch',
+    keywords='renderfarm ncca transfer files watch notification email dropbox',
     packages=find_packages(exclude=['docs', 'test']),
     install_requires=['coloredlogs', 'dropbox', 'pysftp'],
     extras_require={
